63d8dd5546faf03afab80369fee6e4bdc269d288
nurbek2107 <abduaxatov002@gmail.com>
2024-05-22 08:58:38 +0500

    main
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
+fetch("http://localhost:3000/products")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(products => updateProducts(products))
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
 
-const cartBtn = document.getElementById("cart");
-const modal = document.getElementById("modal");
-const addHidden = () => {
-    modal.classList.add("hidden");
-};
-const modalTogle = () => {
-    if (modal.classList.contains("hidden")) {
-        modal.classList.remove("hidden");
-    } else modal.classList.add("hidden");
-};
-cartBtn.addEventListener("click", modalTogle);
-document.addEventListener("keydown", (e) => {
-    if (e.key == "Escape") {
-        addHidden();
+function updateProducts(products) {
+    products.forEach(product => {
+        console.log(product);
+    });
+}
+
+const shop = document.getElementById("cart");
+const el = document.getElementById("car5");
+const bodyel = document.getElementById("body")
+const overlay = document.getElementById('overlay')
+let isPlaying = false;
+
+shop.addEventListener("click", () => {
+    isPlaying = !isPlaying;
+    if (isPlaying) {
+        el.classList.add('visible');
+        const overlay = document.getElementById('overlay')
+    } else {
+        el.classList.remove('visible');
+        const overlay = document.getElementById('overlay')
     }
 });
